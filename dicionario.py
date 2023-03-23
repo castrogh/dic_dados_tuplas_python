13fb5fb83f6266845b7486185059211ce69b5f47
Gabriel Castro <ghscastro@hotmail.com>
2023-03-23 15:20:13 -0300

    Buscando as informações de um usuário, usando o método get.

--- dicionario.py
+++ dicionario.py
@@ -8,3 +8,6 @@ usuarios={ #os objetos (elementos) do dicionário tem sua chave seguida pelos da
 usuarios["maicao"] = ["Maicão Castro", "20/03/2023", "Office_X03"] #ao adicionar desta forma mais um objeto no dicionário, o Python vai interpretá-lo como um novo registro a ser mostrado e não um novo dicionário
 
 print (usuarios)
+
+print ("####----####")
+print (usuarios.get(input("Digite o login que deseja consultar: "))) #o método get traz a informação solicitada pelo usuário, caso ela esteja registrada no dicionário
