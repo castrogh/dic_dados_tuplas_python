02447d7fbe5fc46fe6ab9737dcd7f8f6fb03a66a
Gabriel Castro <ghscastro@hotmail.com>
2023-03-23 15:03:54 -0300

    Criação dos objetos do dicionário.

--- dicionario.py
+++ dicionario.py
@@ -1,2 +1,8 @@
 usuarios = {}
+
+usuarios={ #os objetos (elementos) do dicionário tem sua chave seguida pelos dados, como mostrado abaixo, os dados ficam armazenados numa lista 
+    "castrogh": ["Gabriel Castro", "23/03/2023", "Office_X01"],
+    "danicastro": ["Danielle Castro", "22/03/2023", "Office_X02"]
+}
+
 print (usuarios)
